b266d7e18c54214ee1837d3b591548d1a8e50f70
vperezlobato <55490747+vperezlobato@users.noreply.github.com>
2020-01-10 14:32:51 +0100

    subido

--- PongSignalRUniversal/PongSignalRUniversal/Entidades/objetoJuego.cs
+++ PongSignalRUniversal/PongSignalRUniversal/Entidades/objetoJuego.cs
@@ -9,23 +9,24 @@ namespace PongSignalRUniversal.Entidades
     public class objetoJuego
     {
         public double izquierda { get; set; }
-        public double top { get; set; }
         public string id { get; set; }
 
         public string ultimoActualizado { get; set; }
         public bool seHaMovido { get; set; }
 
         public int velocidad { get; set; }
-        public int posicionX { get; set; }
+        public double posicionY { get; set; }
 
-        public objetoJuego(double izquierda, double top, string id, string ultimoActualizado, bool seHaMovido, int velocidad, int posicionX) {
+        public Uri imagen { get; set; }
+
+        public objetoJuego(double izquierda, string id, string ultimoActualizado, bool seHaMovido, int velocidad, double posicionY,Uri imagen) {
             this.izquierda = izquierda;
-            this.top = top;
             this.id = id;
             this.ultimoActualizado = ultimoActualizado;
             this.seHaMovido = seHaMovido;
+            this.imagen = imagen;
             this.velocidad = velocidad;
-            this.posicionX = posicionX;
+            this.posicionY = posicionY;
         }
     }
 }
--- PongSignalRUniversal/PongSignalRUniversal/Model/MainPageVM.cs
+++ PongSignalRUniversal/PongSignalRUniversal/Model/MainPageVM.cs
@@ -29,9 +29,9 @@ namespace PongSignalRUniversal.Model
             dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
             dispatcherTimer.Tick += timerTick;
 
-            _jugador1 = new objetoJuego(100, 100, "jugador1", "jugador1", false, 50, 100);
-            _jugador2 = new objetoJuego(880, 100, "jugador2", "jugador2", false, 50, 0);
-            _pelota = new objetoJuego(250, 500, "pelota", "pelota", false, 100,200);
+            _jugador1 = new objetoJuego(0, "jugador1", "jugador1", false, 50, 300, new Uri("ms-appx:///Assets/barra.png"));
+            _jugador2 = new objetoJuego(880, "jugador2", "jugador2", false, 50, 300, new Uri("ms-appx:///Assets/barra.png"));
+            _pelota = new objetoJuego(250, "pelota", "pelota", false, 100, 500, null);
             //proxy.Invoke("añadirObjetoJuego",_pelota.id);
             //proxy.Invoke("añadirObjetoJuego",_jugador1.id);
             //proxy.Invoke("añadirObjetoJuego",_jugador2.id);
@@ -86,10 +86,10 @@ namespace PongSignalRUniversal.Model
         /// </summary>
         public void move()
         {
-            Double posicionFutura = _jugador1.posicionX + _jugador1.velocidad;
-            if (posicionFutura > 0 && posicionFutura < 1275)
+            Double posicionFutura = _jugador1.posicionY + _jugador1.velocidad;
+            if (posicionFutura > 0 && posicionFutura < 1000)
             {
-                _jugador1.posicionX += _jugador1.velocidad;
+                _jugador1.posicionY += _jugador1.velocidad;
             }
             NotifyPropertyChanged("jugador1");
         }
@@ -100,7 +100,7 @@ namespace PongSignalRUniversal.Model
         public void right()
         {
             //_velocidad = 10;
-            if (_jugador1.posicionX < 1275)
+            if (_jugador1.posicionY < 1000)
             {
                 _jugador1.velocidad = 10;
             }
@@ -115,7 +115,7 @@ namespace PongSignalRUniversal.Model
         public void left()
         {
             //_velocidad = -10;
-            if (_jugador1.posicionX > 0 && _jugador1.posicionX - 10 > 0)
+            if (_jugador1.posicionY > 0 && _jugador1.posicionY - 10 > 0)
             {
                 _jugador1.velocidad = -10;
             }
